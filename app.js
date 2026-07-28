541f6b885dedfbb938ad9e5376c75b847e0f916a
urbansportsarena20-blip <urbansportsarena20@gmail.com>
2026-07-29 00:56:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ====== CONFIG ======
 // Paste your deployed Apps Script Web App URL below (ends in /exec)
 const CONFIG = {
-  API_URL: 'API_URL: 'https://script.google.com/macros/s/AKfycbw5H8V18If04bbuJUpFRBchuHouZ_Y9VHM0BSzk3VCv5aagPXD1suP45e2hiFpGGWNcRQ/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbw5H8V18If04bbuJUpFRBchuHouZ_Y9VHM0BSzk3VCv5aagPXD1suP45e2hiFpGGWNcRQ/exec'
 };
 
 const BRANCHES = ['Urban Sports Arena', 'Rishikul Vijay', 'Gyanvihar'];
